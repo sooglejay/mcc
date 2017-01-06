09d071c73a26c4c13353602d68983c754fd51794
sooglejay <394055524@qq.com>
2017-01-06 16:51:15 +0800

    add two files

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://182.92.123.78/admin/');
+define('HTTP_CATALOG', 'http://182.92.123.78/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://182.92.123.78/admin/');
+define('HTTPS_CATALOG', 'http://182.92.123.78/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'jiangwei_test');
+define('DB_PASSWORD', 'jiangwei_test');
+define('DB_DATABASE', 'mcc_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
